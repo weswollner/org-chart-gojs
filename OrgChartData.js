c8ae766ac53c5ed14d2208c3c35679df2d240edb
Wes Wollner <weswollner@gmail.com>
2023-03-01 12:25:22 -0500

    inital

---
+++ OrgChartData.js
@@ -0,0 +1,65 @@
+export const orgChartData = [
+	{
+		"id": "001",
+		"name": "Brittney Johnson",
+		"positionTitle": "University Registrar",
+		"location": "Boyd Building, 1011A",
+		"department": "Office of the Registrar",
+		"supervisor": "000",
+		"imageLocation": ""
+	},
+	{
+		"id": "002",
+		"name": "Andy Kline",
+		"positionTitle": "Associate Registrar, Systems",
+		"location": "Boyd Building, 1011B",
+		"department": "Office of the Registrar",
+		"supervisor": "001",
+		"imageLocation": ""
+	},
+	{
+		"id": "003",
+		"name": "Esther Garcia Ramirez",
+		"positionTitle": "Associate Registrar, Academics",
+		"location": "Boyd Building, 1011N",
+		"department": "Office of the Registrar",
+		"supervisor": "001",
+		"imageLocation": ""
+	},
+	{
+		"id": "004",
+		"name": "Taylor Miller",
+		"positionTitle": "Administrative Assistant",
+		"location": "Boyd Building, 1011C",
+		"department": "Office of the Registrar",
+		"supervisor": "001",
+		"imageLocation": ""
+	},
+	{
+		"id": "005",
+		"name": "Micheal Green",
+		"positionTitle": "Sr Reporting Analyst",
+		"location": "Boyd Building, 1012D",
+		"department": "Office of the Registrar",
+		"supervisor": "002",
+		"imageLocation": ""
+	},
+	{
+		"id": "006",
+		"name": "Margret Roberts",
+		"positionTitle": "Administrative Clerk",
+		"location": "Boyd Building, 1010F",
+		"department": "Office of the Registrar",
+		"supervisor": "002",
+		"imageLocation": ""
+	},
+	{
+		"id": "007",
+		"name": "Selasi Ells",
+		"positionTitle": "Special Assistant",
+		"location": "Boyd Building, 1010F",
+		"department": "Office of the Registrar",
+		"supervisor": "002",
+		"imageLocation": ""
+	}
+]
